728a9414200c0f59f704de469b7721e0b1566bdc
Fernando Jorge Gregorio Perez <fernando24gregorio@gmail.com>
2024-05-02 11:39:04 +0200

    update

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -5,10 +5,14 @@ const {
   addUser,
   selectUser,
   updateUser,
+  login,
 } = require('../controllers/user.controller');
 
 router.post('/add', addUser);
 router.get('/select', selectUser);
 router.put('/update/:id', updateUser);
+
+//Loguin de un usuario
+router.post("/login", login )
 
 module.exports = router;
