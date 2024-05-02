3653fea386da6421713afb9c5af047d9680f02e9
Fernando Jorge Gregorio Perez <fernando24gregorio@gmail.com>
2024-05-02 13:06:34 +0200

    update

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -41,18 +41,22 @@ const selectUser = async (req, res) => {
 //Registro de un nuevo usuario
 const register = async (req, res) => {
   try {
+
     // creo el documento del usuario
     const userDoc = new User(req.body);
     console.log(req.body);
+
     //validaciones
     //1.- El usuario no exista. (email)
     const valEmail = await validateEmailDB(req.body.email);
     console.log(valEmail); // devuelve null si no se encuentra  en la BD
     if (!valEmail) {
+
       // valEmail === null
       //2.- La contraseña cumpla el patron requerido (regex)
       const valPassword = validatePassword(req.body.password);
       if (valPassword) {
+        
         //3.- Encriptar la contraseña  antes de registrarme  HASH
         userDoc.password = bcrypt.hashSync(userDoc.password, 10);
         const createdUser = await userDoc.save();
@@ -84,7 +88,7 @@ const login = async (req, res) => {
     if (!bcrypt.compareSync(userBody.password, userDB.password)) {
       return res.status(200).json({ succes: true, message: "Contraseña no válida" })
     }
-    //generar el token
+    //generación del token y ejecución.
     const token = generateToken({
       name: userDB.name,
       email: userDB.email,
@@ -101,7 +105,7 @@ const modifyProfile = async (req, res) => {
   console.log("funcion de modificar")
   console.log(req.userProfile); // es el usuario con los datos correspondiente al token
   const newUser = new User(req.body);
-  newUser.password = bcrypt.hashSync(req.body.password, 10)
+  newUser.password = bcrypt.hashSync(req.body.password, 10) //encriptar contraseña
   newUser._id = req.userProfile._id
   console.log(newUser)
   const updateUser = await User.findByIdAndUpdate(req.userProfile._id, newUser, { new: true })
--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -5,14 +5,18 @@ const {
   addUser,
   selectUser,
   updateUser,
+  register,
   login,
+  modifyProfile
 } = require('../controllers/user.controller');
+const { isAuth } = require("../../middleware/auth")
+
 
 router.post('/add', addUser);
 router.get('/select', selectUser);
 router.put('/update/:id', updateUser);
-
-//Loguin de un usuario
-router.post("/login", login )
+router.post('/register', register);
+router.post('/login', login); //Loguin de un usuario
+router.put('/update', [isAuth], modifyProfile);
 
 module.exports = router;
